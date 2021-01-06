75d5b052fdcb08626c209a51c185fe0bda913930
Roma Karvacky <64963734+Karvacky-Roma@users.noreply.github.com>
2021-01-06 23:06:50 +0200

    Files uploaded

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,24 +1,47 @@
-@@include('slick.min.js')
+function ibg() {
 
-$('.slider__row').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  loop: true
-});
+	$.each($('.ibg'), function (index, val) {
+		if ($(this).find('img').length > 0) {
+			$(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+		}
+	});
+}
+
+ibg();
+
 
 /* --------------- */
 /* --- JQUERY ---- */
 /* --------------- */
 
 
-function ibg() {
+@@include('slick.min.js')
 
-    $.each($('.ibg'), function (index, val) {
-        if ($(this).find('img').length > 0) {
-            $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
-        }
-    });
-}
-
-ibg();
+$('.slider__row').slick({
+	speed: 400,
+	lazyLoad: 'ondemand',
+	draggable: false,
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	loop: true,
+	responsive: [
+    {
+      breakpoint: 745,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 396,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
--- Smart-Notes/js/main.js
+++ Smart-Notes/js/main.js
@@ -1,10 +1,10 @@
 function ibg() {
 
-    $.each($('.ibg'), function (index, val) {
-        if ($(this).find('img').length > 0) {
-            $(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
-        }
-    });
+	$.each($('.ibg'), function (index, val) {
+		if ($(this).find('img').length > 0) {
+			$(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+		}
+	});
 }
 
 ibg();
@@ -19,8 +19,30 @@ ibg();
 
 
 $('.slider__row').slick({
-  infinite: true,
-  slidesToShow: 3,
-  slidesToScroll: 1,
-  loop: true
+	speed: 400,
+	lazyLoad: 'ondemand',
+	draggable: false,
+	infinite: true,
+	slidesToShow: 3,
+	slidesToScroll: 1,
+	loop: true,
+	responsive: [
+    {
+      breakpoint: 745,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 396,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
 });
