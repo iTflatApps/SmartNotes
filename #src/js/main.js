161d9be61879414bdc59ea0e8f4e19e3eba915b9
Roma Karvacky <64963734+Karvacky-Roma@users.noreply.github.com>
2021-01-07 15:31:39 +0200

    Added animation for all elements

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,6 +1,6 @@
-/* ----------------------- */
-/* ------- JQUERY -------- */
-/* ----------------------- */
+/* --------------------------------------------------------------------- */
+/* ------------------------------ JQUERY ------------------------------- */
+/* --------------------------------------------------------------------- */
 
 
 @@include('slick.min.js')
@@ -31,25 +31,14 @@ $('.slider__row').slick({
   ]
 });
 
-/* ----------------------- */
-/* ------- JQUERY -------- */
-/* ----------------------- */
+/* --------------------------------------------------------------------- */
+/* ------------------------------ JQUERY ------------------------------- */
+/* --------------------------------------------------------------------- */
 
 
 
-function ibg() {
 
-	$.each($('.ibg'), function (index, val) {
-		if ($(this).find('img').length > 0) {
-			$(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
-		}
-	});
-}
-
-ibg();
-
-
-
+// ANIMATION HEADER ------------------------------------
 
 
 let scrollpos = window.scrollY;
@@ -67,5 +56,77 @@ window.addEventListener('scroll', function() {
   if (scrollpos >= header_height) { add_class_on_scroll() }
   else { remove_class_on_scroll() }
 
-  console.log(scrollpos)
+  // console.log(scrollpos)
 })
+
+
+// ANIMATION WHEN SCROLL ------------------------------
+
+
+const animItems = document.querySelectorAll('._anim-items');
+
+if (animItems.length > 0) {
+  window.addEventListener('scroll', animOnScroll);
+  function animOnScroll() {
+    for (let index = 0; index < animItems.length; index++) {
+      const animItem = animItems[index];
+      const animItemHeight = animItem.offsetHeight;
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+
+      if(animItemHeight > window.innerHeight ){
+        animItemPoint = window.innerHeight - animItemHeight / animStart;
+      }
+
+      if ((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)) {
+        animItem.classList.add("_active");
+      }else{
+        if (!animItem.classList.contains("_anim-no-hide")) {
+          animItem.classList.remove("_active");
+        }
+      }
+    }
+  }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+  }
+
+  setTimeout(() =>{
+    animOnScroll();
+  }, 300) 
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
