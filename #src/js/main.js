e1f776281a23498a9295fc7aa0e5b099f6e2ac94
Roma Karvacky <64963734+Karvacky-Roma@users.noreply.github.com>
2021-01-07 11:32:03 +0200

    Added basic animation & transition

--- #src/js/main.js
+++ #src/js/main.js
@@ -1,31 +1,19 @@
-function ibg() {
-
-	$.each($('.ibg'), function (index, val) {
-		if ($(this).find('img').length > 0) {
-			$(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
-		}
-	});
-}
-
-ibg();
-
-
-/* --------------- */
-/* --- JQUERY ---- */
-/* --------------- */
+/* ----------------------- */
+/* ------- JQUERY -------- */
+/* ----------------------- */
 
 
 @@include('slick.min.js')
 
 $('.slider__row').slick({
-	speed: 400,
-	lazyLoad: 'ondemand',
-	draggable: false,
-	infinite: true,
-	slidesToShow: 3,
-	slidesToScroll: 1,
-	loop: true,
-	responsive: [
+  speed: 400,
+  lazyLoad: 'ondemand',
+  draggable: false,
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  loop: true,
+  responsive: [
     {
       breakpoint: 745,
       settings: {
@@ -40,8 +28,44 @@ $('.slider__row').slick({
         slidesToScroll: 1
       }
     }
-    // You can unslick at a given breakpoint now by adding:
-    // settings: "unslick"
-    // instead of a settings object
   ]
 });
+
+/* ----------------------- */
+/* ------- JQUERY -------- */
+/* ----------------------- */
+
+
+
+function ibg() {
+
+	$.each($('.ibg'), function (index, val) {
+		if ($(this).find('img').length > 0) {
+			$(this).css('background-image', 'url("' + $(this).find('img').attr('src') + '")');
+		}
+	});
+}
+
+ibg();
+
+
+
+
+
+let scrollpos = window.scrollY;
+const header = document.querySelector(".header");
+const header_height = header.offsetHeight;
+
+
+const add_class_on_scroll = () => header.classList.add("active")
+const remove_class_on_scroll = () => header.classList.remove("active")
+
+
+window.addEventListener('scroll', function() { 
+  scrollpos = window.scrollY
+
+  if (scrollpos >= header_height) { add_class_on_scroll() }
+  else { remove_class_on_scroll() }
+
+  console.log(scrollpos)
+})
