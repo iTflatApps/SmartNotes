e111b5e65bb2af89c1fd50a83d5ec6bfd3b0b6c6
Karvacky-Roma <karvatskij.roma@gmail.com>
2021-06-20 18:33:58 +0300

    Remove the gulp-fonter function

--- gulpfile.js
+++ gulpfile.js
@@ -9,21 +9,18 @@ let path = {
         css: project_folder + "/css/",
         js: project_folder + "/js/",
         img: project_folder + "/img",
-        fonts: project_folder + "/fonts/"
     },
     src: {
         html: [source_folder + "/*.html", "!" + source_folder + "/_*.html"],
         css: source_folder + "/scss/style.scss",
         js: source_folder + "/js/main.js",
         img: source_folder + "/img/**/*.{jpg,png,gif,svg,ico,webp}",
-        fonts: source_folder + "/fonts/*.ttf",
     },
     watch: {
         html: source_folder + "/**/*.html",
         css: source_folder + "/scss/**/*.scss",
         js: source_folder + "/js/**/*.js",
         img: source_folder + "/img/**/*.{jpg,png,gif,svg,ico,webp}",
-        fonts: source_folder + "/fonts/*.ttf",
     },
     clean: "./" + project_folder + "/"
 }
@@ -41,11 +38,7 @@ let { src, dest } = require('gulp'),
     uglify = require('gulp-uglify-es').default,
     imagemin = require('gulp-imagemin'),
     webp = require('gulp-webp'),
-    webpHTML = require('gulp-webp-html'),
-    ttf2woff = require('gulp-ttf2woff'),
-    ttf2woff2 = require('gulp-ttf2woff2'),
-    fonter = require('gulp-fonter');
-
+    webpHTML = require('gulp-webp-html')
 
 
 function browserSync(params) {
@@ -123,15 +116,6 @@ function clean() {
     return del(path.clean);
 }
 
-function fonts() {
-    src(path.src.fonts)
-        .pipe(ttf2woff())
-        .pipe(dest(path.build.fonts));
-    return src(path.src.fonts)
-        .pipe(ttf2woff2())
-        .pipe(dest(path.build.fonts));
-}
-
 function cb() { }
 
 function watchFiles() {
@@ -141,10 +125,9 @@ function watchFiles() {
         gulp.watch([path.watch.img], images);
 }
 
-let build = gulp.series(clean, gulp.parallel(js, css, html, images, fonts));
+let build = gulp.series(clean, gulp.parallel(js, css, html, images));
 let watch = gulp.parallel(build, browserSync, watchFiles);
 
-exports.fonts = fonts;
 exports.images = images;
 exports.js = js;
 exports.css = css;
