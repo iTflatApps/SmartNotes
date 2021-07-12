98eb4027bf9cde5a20cb9f14d7dc9bdd29cf6a8d
Karvacky-Roma <karvatskij.roma@gmail.com>
2021-07-13 01:00:25 +0300

    Revert "header redesign"

--- SmartNotes/js/main.js
+++ SmartNotes/js/main.js
@@ -55,17 +55,3 @@ if (animItems.length > 0) {
     animOnScroll();
   }, 300) 
 }
-
-// Header burger
-
-const headerList = document.querySelector(".header__list");
-const headerBurger = document.querySelector(".header__burger");
-const body = document.querySelector("body");
-
-
-headerBurger.addEventListener("click", function (e) {
-    e.preventDefault();
-    headerList.classList.toggle("header-menu-active");
-    headerBurger.classList.toggle("header-menu-active");
-    body.classList.toggle("lock");
-})
