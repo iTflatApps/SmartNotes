f76055a81fd70991c0c7c4ec5df264994e30b6dd
Karvacky-Roma <karvatskij.roma@gmail.com>
2021-07-13 22:03:26 +0300

    Header redesign

--- #src/js/main.js
+++ #src/js/main.js
@@ -55,3 +55,17 @@ if (animItems.length > 0) {
     animOnScroll();
   }, 300) 
 }
+
+// Burger menu
+
+const headerList = document.querySelector(".header__list");
+const headerBurger = document.querySelector(".header__burger");
+const body = document.querySelector("body");
+
+
+headerBurger.addEventListener("click", function (e) {
+    e.preventDefault();
+    headerList.classList.toggle("header-menu-active");
+    headerBurger.classList.toggle("header-menu-active");
+    body.classList.toggle("lock");
+})
